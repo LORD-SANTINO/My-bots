8c3f2ae31d637c89c2f6bc7a529d5acf6a802e63
LORD-SANTINO <mikkyrose297@gmail.com>
2025-08-16 22:32:41 +0100

    Create Footer.js

---
+++ components/Footer.js
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer style={{ marginTop: "2rem", padding: "1rem 0", textAlign: "center", color: "#888" }}>
+      © {new Date().getFullYear()} Your Name. All rights reserved.
+    </footer>
+  );
+}
