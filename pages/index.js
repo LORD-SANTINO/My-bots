7e3d0b38977fbcf9042e3100f386778f9c46f7f0
LORD-SANTINO <mikkyrose297@gmail.com>
2025-08-16 23:53:44 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ import BotCard from "../components/BotCard";
 export default function Home() {
   return (
     <main style={{ padding: 24 }}>
-      <h1>My Bot Portfolio</h1>
+      <h1>DAX BOTS</h1>
 
       <div style={{ display: "grid", gap: 16, gridTemplateColumns: "repeat(auto-fill, minmax(260px, 1fr))" }}>
         {bots?.map((bot) => (
