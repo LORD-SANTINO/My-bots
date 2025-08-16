274e3824ed8a47e01a531ef58896aa76b24fb1af
LORD-SANTINO <mikkyrose297@gmail.com>
2025-08-17 00:01:02 +0100

    Update about.js

--- pages/about.js
+++ pages/about.js
@@ -10,8 +10,8 @@ export default function About() {
         I love building smart, creative, and useful bots with Python. Explore my portfolio to see what I’ve made/done!
       </p>
       <ul>
-        <li>GitHub: <a href="https://github.com/LORD-SANTINO" target="_blank" rel="noopener noreferrer">LORD-SANTINO/a></li>
-        <li>Telegram: <a href="https://t.me/Daxhr" target="_blank" rel="noopener noreferrer">HIM IS DAX⚡</a>HIM IS DAX⚡</li>
+        <li>GitHub: <a href="https://github.com/LORD-SANTINO" target="_blank" rel="noopener noreferrer">LORD-SANTINO</a></li>
+        <li>Telegram: <a href="https://t.me/Daxhr" target="_blank" rel="noopener noreferrer">HIM IS DAX⚡</a></li>
         <li>YouTube Channel: <a href="https://" target="_blank" rel="noopener noreferrer"></a></li>
       </ul>
     </main>
