275070f075dc93a5f249f25e6da35fe212276c10
LORD-SANTINO <mikkyrose297@gmail.com>
2025-08-17 00:08:51 +0100

    Update bots.js

--- data/bots.js
+++ data/bots.js
@@ -2,5 +2,7 @@
 const bots = [
   { id: "grp-helper", name: "Group Helper", description: "Moderation & utilities for Telegram groups.", link: "https://t.me/grphelper_bot?start=_tgr_ovnQ8FthYmQ0" },
   { id: "Dax Gpt", name: "Dax Gpt", description: "An all-in-one AI bot! 🤖 Chat with Chatgpt AI, generate images, upload to Catbox, get IP info and many more.", link: "https://t.me/daxgpt_bot?start=_tgr_Ka6XxGcyOGU0" }
+  { id: "dax oracle", name: "Dax Oracle", description: "Ｄᗩ𐊴 OᖇᗩᑕᒪE – Your stealthy command hub. Automate actions, run utilities, and stay ahead in the digital world. Powered by precision.", link: "https://t.me/DAXORC_BOT?start=_tgr_wlLZ2xgzYTZk" },
+  { id: "group manager", name: "Group Manager", description: "Group management made easy! Plus, fun games to interact with members inside.", link: "https://t.me/grpmanagebot?start=_tgr_4j2EMOJiNmZk" }
 ];
 export default bots;
