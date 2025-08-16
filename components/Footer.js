11667e6709cd9b04686ce8c66da90feee2fa78d1
LORD-SANTINO <mikkyrose297@gmail.com>
2025-08-16 23:39:47 +0100

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer style={{ marginTop: "2rem", padding: "1rem 0", textAlign: "center", color: "#888" }}>
-      © {new Date().getFullYear()} Your Name. All rights reserved.
+      © {new Date().getFullYear()} DAX-NETWORK. All rights reserved.
     </footer>
   );
 }
