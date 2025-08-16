dd9c2c3211cc5db42398d0f6af5b470de45d5ce1
LORD-SANTINO <mikkyrose297@gmail.com>
2025-08-16 22:34:38 +0100

    Create blogs.js

---
+++ data/blogs.js
@@ -0,0 +1,17 @@
+// /data/blogs.js
+
+const blogs = [
+  {
+    id: "hello-world",
+    title: "Welcome to My Bot Blog!",
+    date: "2025-08-16",
+    summary: "Kickstarting my development blog to share tips, updates, and stories.",
+    content: `
+      <p>Hello, world! This is my very first blog post. Here, I'll share updates on new bots, fun projects, and tips for fellow devs.</p>
+      <p>Stay tuned for more!</p>
+    `
+  },
+  // Add more blog posts here
+];
+
+export default blogs;
