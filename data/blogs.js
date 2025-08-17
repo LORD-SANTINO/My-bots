a208e9d1c5139b416f4e7595603bd6f77354c0d9
LORD-SANTINO <mikkyrose297@gmail.com>
2025-08-17 01:10:47 +0100

    Update blogs.js

--- data/blogs.js
+++ data/blogs.js
@@ -1,17 +1,16 @@
-// /data/blogs.js
-
 const blogs = [
   {
     id: "hello-world",
     title: "Welcome to My Bot Blog!",
-    date: "2025-08-16",
+    author: "Admin",
+    date: "2025-08-16T00:00:00Z",
     summary: "Kickstarting my development blog to share tips, updates, and stories.",
     content: `
       <p>Hello, world! This is my very first blog post. Here, I'll share updates on new bots, fun projects, and tips for fellow devs.</p>
       <p>Stay tuned for more!</p>
     `
   },
-  // Add more blog posts here
+  // Add more static posts here
 ];
 
 export default blogs;
